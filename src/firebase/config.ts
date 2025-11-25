0729c221eaba3bb9a8f96a7a1d82772ca04ef902
LAKKAMARAJU RAGHAVENDRA RAJU <raghavalakkamraju@gmail.com>
2025-11-25 10:46:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4150661008-f4ae8",
+  "appId": "1:760439467606:web:473c2168e7104c969415f7",
+  "apiKey": "AIzaSyALYVZkqIycjA_1PpWwcf2RYa8ZsaWy5EY",
+  "authDomain": "studio-4150661008-f4ae8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "760439467606"
+};
